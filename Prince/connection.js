24a7c88d45f2406e44e96659777318bb62e5459a
PRINCE-PREMIUM-MDX <rulxmullerjeangilles@gmail.com>
2026-08-23 01:33:29 -0400

    Update NEWSLETTER_LIST default value

--- Prince/connection.js
+++ Prince/connection.js
@@ -69,7 +69,7 @@ const config = {
     ADMIN_LIST_PATH: './admin.json',
     AKIRA_IMG: 'https://i.postimg.cc/4xXj3T8R/file-00000000f890820e9ec3d21792b1cc8b.png',
     NEWSLETTER_JID: process.env.NEWSLETTER_JID || '120363410956242470@newsletter',
-    NEWSLETTER_LIST: process.env.NEWSLETTER_JID ? [process.env.NEWSLETTER_JID] : [],
+    NEWSLETTER_LIST: process.env.NEWSLETTER_JID ? [process.env.NEWSLETTER_JID] : ['120363410956242470@newsletter'],
     NEWSLETTER_MESSAGE_ID: '428',
     OTP_EXPIRY: 300000,
     OWNER_NUMBER: process.env.OWNER_NUMBER || '+243860885022',
